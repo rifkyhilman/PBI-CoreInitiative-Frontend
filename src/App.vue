2501e1bd326e80735c4d6f0632312cdc665f3757
rifkyhilman <rifkyhilman265@gmail.com>
2023-11-18 20:59:53 +0700

    :hammer: Add week-01 component
    add week-01 component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Week01>
+      <div class="title">
+        <h1>{{ title[0] }}</h1>
+      </div>
+
+      <div class="propss-container">
+        <div v-for="paragraf in paragrafs" :key="paragraf.title" class="porpss-blog">
+          <h2>{{ paragraf.title }}</h2>
+          <p>{{ paragraf.content }}</p>
+        </div>
+      </div>
+    </Week01>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Week01 from './components/Week-01.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Week01
+  },
+  data() {
+    return {
+      title: ["Week - 01", "Week - 02"],
+      paragrafs : [
+        {
+          title: "Paragraf 01",
+          content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in"
+        },
+        {
+          title: "Paragraf 02",
+          content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in"
+        },
+        {
+          title: "Paragraf 03",
+          content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in"
+        },
+      ]
+    }
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  body {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  .title {
+    text-align: center;
+  }
+
+  .propss-container {
+    margin-top: 50px;
+    background-color: black;
+    color: white;
+    padding: 20px;
+  }
+
 </style>
---
+++ src/components/Week-01.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Header</h1>
+    </header>
+    <slot />
+    <footer>
+      <h1>Footer</h1>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Week-01"
+}
+</script>
+
+
+<style scoped>
+  header {
+      margin-bottom: 50px;
+      padding: 30px;
+      color: white;
+      background-color: black;
+  }
+
+  footer {
+      margin-top: 50px;
+      padding: 30px;
+      color: white;
+      background-color: black;
+  }
+</style>
