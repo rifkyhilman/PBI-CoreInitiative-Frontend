5b9b196cf03d8f84cd4099df7082fb606405fc5e
rifkyhilman <rifkyhilman265@gmail.com>
2023-11-18 18:55:33 +0700

    :tada: Setup and github workfow
    setup and github workflow

---
+++ minggu-02/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <ul>
+      <li v-for="book in myBooks" :key="book.title">
+          <h1> {{ book.title }} </h1>
+          <p> {{ book.author }} </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      myBooks: [
+        {
+          title  : "Makan Malam",
+          author : "Rifki"
+        },
+        {
+          title  : "Makan Siang",
+          author : "Hilman"
+        },
+        {
+          title  : "Makan Pagi",
+          author : "Bestrad"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
